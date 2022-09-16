0143acd11ce1d2c3b6204a1f61dc4318e4ab1873
Moriound <3217901841@qq.com>
2022-09-16 15:14:04 +0800

    2nd commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # np-mpl
+##numpy和matplotlib的学习
 
 ### numpy_test.py 
 #### &emsp;numpy的一些基本内容
@@ -8,7 +9,10 @@
 #### &emsp;从数值范围创建numpy数组
 
 
+### numpy_ 从数值范围创建数组.py
+#### &emsp;单个图表
 
+![mpl-1.png](https://github.com/Moriound/np-mpl/blob/master/img/mpl-1.png?raw=true)
 
 
 
--- matplotlib-1.py
+++ matplotlib-1.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # test 1
-"""plt.title("learning")
+plt.title("learning")
 plt.xlabel("x-x ")
 plt.ylabel("y-F(x)")
 plt.grid(color='lightgreen', axis='both', linestyle='dashed')
@@ -16,22 +16,5 @@ plt.plot(x, y, "b")
 plt.plot(y3)
 plt.plot(x, y2, linestyle='dashdot', linewidth=10)
 plt.show()
-"""
 
-# test2
-# 多图
-# plt.subplots(2, 3)  # 一次创建多图
-# plt.subplot(2, 3, 4)  # (几行， 几列，总的第几个)
 
-# 先选择再绘图
-plt.subplot(2,3,6)
-
-plt.plot([1,2], [3, 4])
-
-plt.subplot(2,3,1)
-
-x = np.linspace(-5,5,100)
-y = np.sin(x)
-plt.plot(x, y)
-
-plt.show()
